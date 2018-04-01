a02c391f0f78675425a1ad44f77f7f8e422a3db3
patrick <patrick.virtanen@gmail.com>
2018-04-01 17:47:30 +0200

    Added permission to internet and an Asynchronous get!

--- app/src/main/java/com/example/patrick/stormy2/MainActivity.java
+++ app/src/main/java/com/example/patrick/stormy2/MainActivity.java
@@ -2,14 +2,60 @@ package com.example.patrick.stormy2;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
+
+        import java.io.IOException;
+
+        import okhttp3.Call;
+        import okhttp3.Callback;
+        import okhttp3.OkHttpClient;
+        import okhttp3.Request;
+        import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "89f39ad8c11fa010586381e6207d9d55";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forcastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "/" + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forcastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught", e);
+                }
+
+
+            }
+
+        });
+
+        Log.d(TAG, "Main UI code is running");
     }
 }
 
-// Hej hej nu testar vi
+// Handling errors
+
